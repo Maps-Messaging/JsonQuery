b318220f2b3ce5047ba17e4449ca2b8e302bf303
Matthew Buckton <matthew@buckton.org>
2026-01-05 16:20:45 +1100

    update copyright

--- src/main/java/io/mapsmessaging/jsonquery/JsonQueryParser.java
+++ src/main/java/io/mapsmessaging/jsonquery/JsonQueryParser.java
@@ -1,3 +1,22 @@
+/*
+ *
+ *  Copyright [ 2020 - 2024 ] Matthew Buckton
+ *  Copyright [ 2024 - 2026 ] MapsMessaging B.V.
+ *
+ *  Licensed under the Apache License, Version 2.0 with the Commons Clause
+ *  (the "License"); you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://commonsclause.com/
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 // File: JsonQueryParser.java
 package io.mapsmessaging.jsonquery;
 
@@ -1001,5 +1020,9 @@ public final class JsonQueryParser {
 
   private static boolean isDigit(char ch) {
     return ch >= '0' && ch <= '9';
+  }
+
+  public static void main(String[] args){
+    System.out.println(JsonQueryParser.parse("pick(\"timestamp\", \"particles_gt_10\") | wrap(\"payload\")"));
   }
 }
